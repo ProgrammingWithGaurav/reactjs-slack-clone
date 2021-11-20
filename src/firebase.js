85b04585252d09535734b2ac881ca91117eda178
ProgrammingWithGaurav <gaurav2499kumar@gmail.com>
2021-11-20 21:39:39 +0530

    Adding sidebarOptio

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA36UeaAy7aJ-fsDoJMMd2OHopxe9lr0ew",
+    authDomain: "reactprojects-12bbe.firebaseapp.com",
+    projectId: "reactprojects-12bbe",
+    storageBucket: "reactprojects-12bbe.appspot.com",
+    messagingSenderId: "742066518833",
+    appId: "1:742066518833:web:7f565f0b1a180e549d4e10",
+    measurementId: "G-B8CMM569VH"
+  };
