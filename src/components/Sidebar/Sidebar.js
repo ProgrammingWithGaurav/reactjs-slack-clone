c46e49a5652845a7d7823711a7245bbef5a798d1
ProgrammingWithGaurav <gaurav2499kumar@gmail.com>
2021-11-20 16:30:45 +0530

    Styling sidebar header component

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,10 +1,21 @@
-import React from 'react'
-import "./Sidebar.css"
+import React from 'react';
+import "./Sidebar.css";
+import FiverManualRecordIcon from "@material-ui/icons/FiberManualRecord";
+import CreateIcon from "@material-ui/icons/Create"
 
 function Sidebar() {
     return (
         <div className="sidebar">
-            
+            <div className="sidebar-header">
+                <div className="sidebar-info">
+                    <h2>Gaurav</h2>
+                    <h3>
+                        <FiverManualRecordIcon />
+                        Gaurav
+                    </h3>
+                </div>
+                <CreateIcon />
+            </div>
         </div>
     )
 }
